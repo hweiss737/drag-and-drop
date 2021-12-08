c1f007525ba1c61af55fdf9e7c4d48f4e7ade954
Admin <admin@Athenas-MacBook-Air.local>
2021-12-07 17:29:06 -0700

    cleanup

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path')
 
 const app = express()
 
-//here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
